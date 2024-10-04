933351a13665534a79e07228d0e7ef066e1ac385
hira353 <hiramanik451@gmail.com>
2024-10-04 17:19:05 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -70,11 +70,11 @@ DEVELOPER :Mohammad Nayan
 
 🟣Facebook Account Link: 
 
-https://www.facebook.com/www.xnxx.com169
+https://https://www.facebook.com/profile.php?id=100094456665588
 
-🔵WHATSAPP NUMBER: wa.me/+8801615298449
+🔵WHATSAPP NUMBER: wa.me/+8801863905682
 
-🟢SUPPORT EMAIL: www.mdmnnm2004@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
+🟢SUPPORT EMAIL: hiramanik451@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
   }})
 .catch(error => {
     console.error(error);
